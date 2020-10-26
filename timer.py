e01087d183b51ac4d7e8c6d6f288ccf4ad40fac4
Gughan Ravikumar <57280279+SvbZ3r0@users.noreply.github.com>
2020-10-26 00:16:30 -0700

    Many improvements

--- timer.py
+++ timer.py
@@ -1,11 +1,33 @@
-from time import perf_counter
+from time import perf_counter, process_time
+from functools import partial, wraps
 
-def timer(func):
+fn = {	
+		'sys': perf_counter, 
+		'pro': process_time
+	 }
 
-	def wrapper(*args):
-		t = perf_counter()
-		res = func(*args)
-		print(func.__name__, perf_counter()-t)
+def get_fn(ctr):
+	try:
+		return fn[ctr]
+	except KeyError as e:
+		# from https://stackoverflow.com/a/6062799
+		import sys
+		raise ValueError('ctr only takes arguments: \'sys\' and \'pro\'').with_traceback(sys.exc_info()[2])
+
+def timer(func=None, ctr='sys'):
+	''' Wrapper to time a function '''
+
+	# double wrapper logic from https://stackoverflow.com/a/39335652
+	if func is None:
+		 return partial(timer, ctr=ctr)
+
+	tm = get_fn(ctr)
+
+	@wraps(func)
+	def wrapper(*args, **kwargs):
+		t = tm()
+		res = func(*args, **kwargs)
+		print(func.__name__, tm()-t)
 		return res
 
 	return wrapper
@@ -18,27 +40,30 @@ from time import strftime, localtime
 
 def secondsToStr(elapsed=None):
 	if elapsed is None:
-		return strftime("%Y-%m-%d %H:%M:%S", localtime())
+		return strftime('%Y-%m-%d %H:%M:%S', localtime())
 	else:
 		return str(timedelta(seconds=elapsed))
 
-def startlog():
+def startlog(ctr='sys'):
 	global start
-	start = perf_counter()
-	atexit.register(endlog)
-	log("Start Program")
+	global tm
 
-def log(s, elapsed=None):
-	line = "="*40
+	tm = get_fn(ctr)
+	start = tm()
+	atexit.register(endlog)
+	log('Start Program', elapsed=False)
+
+def log(s, elapsed=True):
+	global start
+	global tm
+	line = '='*40
 	print(line)
-	print(secondsToStr(), '-', s)
+	print(f'{secondsToStr()} - {s}')
 	if elapsed:
-		print("Elapsed time:", elapsed)
+		print(f'Elapsed time: {secondsToStr(tm() - start)}')
 	print(line)
 	print()
 
 def endlog():
-	global start
-	end = perf_counter()
-	elapsed = end-start
-	log("End Program", secondsToStr(elapsed))
+	atexit.unregister(endlog)
+	log('End Program')
